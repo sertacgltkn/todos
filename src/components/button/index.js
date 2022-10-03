4c27779822f0c82d4d8d63e2edccbb423b1d6d2f
Sertaç Gültekin <sertacgltkn@gmail.com>
2022-10-03 19:58:47 +0300

    reached edit button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Button (props) {
-    return <button onClick={props.onClick}>{props.children}</button>
+export default function Button (className="btn btn-default",...props) {     // ...props ile tüm propsları çağırdık ve className default olarak bootstrap butonu ayarladık.
+    return <button onClick={props.onClick} className={className}>{props.children}</button>
 }
--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import classes from "./style.module.css";       //classes ile module.css importu gerçekleştirildi.
+import Button from "../button";
 
 const url = "https://jsonplaceholder.typicode.com/todos";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
+  const [selectedTodo, setSelectedTodo] = useState();
+  
 
   useEffect(() => {                         //API'lere istek atmak için önerilen yöntem useEffect hookudur.
     fetch(url)                              // fetch url ile çağrı yaptık.            
@@ -16,10 +19,9 @@ const TodoList = () => {
       })
   }, []);
 
-  return (
-    <div className={classes.container}> {/* hazırladığımız style class'ını containera işliyoruz. */}
-    <table>
-        <thead>
+  const renderThead = () => {
+    return (
+    <thead>
             <tr>
             <th>
                 ID
@@ -35,7 +37,25 @@ const TodoList = () => {
             </th>
             </tr>
         </thead>
-        <tbody>
+  )}
+
+  const remove = (todo) => {
+    if (window.confirm("Silmek üzeresiniz emin misiniz?")) {
+      setTodos(prev => {
+        return prev.filter(x => x.id != todo.id)
+      })
+    }
+  }
+
+  const edit = (todo) => {
+    setSelectedTodo(todo);
+    
+
+  }
+
+  const renderTbody = () => {
+    return (
+      <tbody>
         {todos.slice(0, 15).map((todo, index) => (         /* önce state olan todos'umuzdan slice methodunun sağladığı aralık parametresi ile
                                                             0 ile 15 arasında göstereceğimiz sayfaadki madde sayısını belirttik. */   
             <tr key={index}>                                   {/* map ile de ekrana yazdırma hazırlığını yaptık  */}
@@ -49,10 +69,12 @@ const TodoList = () => {
                 {todo.completed ? "tamamlandı" : "yapılacak"}
                 </td>
                 <td>
-                    <button>
+                    <button className={`btn btn-sm btn-danger ${classes.actionButton}`}
+                            onClick={() => remove(todo)}>
                         sil    
                     </button>
-                    <button>
+                    <button className="btn btn-sm btn-warning" 
+                            onClick={() => edit(todo) }>
                     düzenle
                     </button>
                 </td>
@@ -61,6 +83,27 @@ const TodoList = () => {
         ))}                                                     
             
         </tbody>
+  )}
+
+      const renderEditForm = () => {
+        return (
+          <div>
+            <input type={"text"}/>
+            <input type="check"/>
+            <button onClick={() => setSelectedTodo(todos)} className="btn btn-sm btn-success">Kaydet</button>
+            <button onClick={() => setSelectedTodo(undefined)} className="btn btn-sm btn-info">Vazgeç</button>
+          </div>
+        )
+      }
+
+  return (
+    <div className= {`${classes.container} container`}> {/* hazırladığımız style class'ını containera işliyoruz. */}
+    {selectedTodo && renderEditForm()}
+    <table className="table">
+      {renderThead()}
+      {renderTbody()}
+      
+        
         </table>                                        
                                                                
     </div>
